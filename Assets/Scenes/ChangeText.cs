7e6d84250630c46d69dec88aefcbf8a94f1b1e4d
Marc Hewitt <marc@marchewitt.com>
2022-02-06 13:53:11 -0600

    Intial commit

---
+++ Assets/Scenes/ChangeText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ChangeText : MonoBehaviour
+{
+    private Text TextRef;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        TextRef = this.gameObject.GetComponent<Text>();
+        Assert.IsNotNull(TextRef, "TextRef can not be null");
+        ChangeTheText("Hello");
+    }
+
+    private void ChangeTheText(string newText)
+    {
+        TextRef.text = newText;
+    }
+}
